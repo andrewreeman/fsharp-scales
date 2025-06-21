a2922931a153c4c8bdb3dfeb5969a5c3ae10f0d4
Andrew Reeman <andrew.reeman@gmail.com>
2025-06-21 21:49:06 +0100

    updated endpoint path

--- scales.api/Controllers/ExerciseController.fs
+++ scales.api/Controllers/ExerciseController.fs
@@ -5,14 +5,14 @@ open ScalePicker
 
 [<ApiController>]
 [<Route("[controller]")>]
-type ScaleController() =
+type ExerciseController() =
     inherit ControllerBase()
 
-    [<HttpGet("")>]
+    [<HttpGet("scale")>]
     member _.GetRandomScale() = ScalePicker.getRandomScale ()
 
     [<HttpGet("arpeggio")>]
     member _.GetRandomArpeggio() = ArpeggioPicker.getRandomArpeggio ()
 
-    [<HttpGet("exercise")>]
+    [<HttpGet("")>]
     member _.GetRandomExercise() = ExercisePicker.getRandomExercise ()
